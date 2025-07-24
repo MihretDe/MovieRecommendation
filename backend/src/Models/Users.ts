fd801347f8b8eb6f79ff09205e287df91f14737d
MihretDe <mihretdesalegn98@gmail.com>
2025-07-24 22:39:47 +0300

    feat(user) add name to the model

--- backend/src/Models/Users.ts
+++ backend/src/Models/Users.ts
@@ -1,13 +1,16 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IUser extends Document {
   email: string;
   auth0Id: string;
+  name: string; // add name field
 }
 
 const UserSchema: Schema = new Schema({
   email: { type: String, required: true, unique: true },
   auth0Id: { type: String, required: true, unique: true },
+  name: { type: String, required: true }, // add name field
 });
 
-export const User = mongoose.models.User || mongoose.model<IUser>('User', UserSchema);
+export const User =
+  mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
